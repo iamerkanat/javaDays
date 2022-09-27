152ad304812d777b80ff8bb43e5b241ba12264fb
Erkanat <iamerkanat@gmail.com>
2022-09-27 18:12:46 +0600

    h

--- src/main/java/aboutString/stringTwo.java
+++ src/main/java/aboutString/stringTwo.java
@@ -1,13 +1,12 @@
 package aboutString;
 
-public class StringOne {
+public class stringTwo {
     public static void main(String[] args) {
         String myString = new String("keep on coding");
         String myString2 = "keep on coding";
 
-        if(myString.equals(myString2)){
-            System.out.println("Strings are equal");
+        System.out.println("Strings are equal");
         }
 
     }
-}
+
